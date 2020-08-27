bf9fc294de8eec529e2756155009e24710de16e7
Quang Ha Bui <ha_bq@septeni-technology.jp>
2020-08-27 16:26:37 +0700

    example

--- examples/todos/src/components/Todo.js
+++ examples/todos/src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Todo = ({ onClick, completed, text }) => (
-  <li
+  <li 
     onClick={onClick}
     style={{
       textDecoration: completed ? 'line-through' : 'none'
--- examples/todos/src/containers/AddTodo.js
+++ examples/todos/src/containers/AddTodo.js
@@ -4,17 +4,18 @@ import { addTodo } from '../actions'
 
 const AddTodo = ({ dispatch }) => {
   let input
+  let submitForm = (e) => {
+    e.preventDefault()
+    if (!input.value.trim()) {
+      return
+    }
+    dispatch(addTodo(input.value))
+    input.value = ''
+  }
 
   return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
+      <form onSubmit={submitForm}>
         <input ref={node => input = node} />
         <button type="submit">
           Add Todo
